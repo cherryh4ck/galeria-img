2a3f8dd894de8882b27d66ef88e39803baafc5a7
toma$ <tomas1234567891016@gmail.com>
2025-05-10 20:23:36 -0300

    Autenticación por array
    permite multiples cuentas

--- php/limpiar.php
+++ php/limpiar.php
@@ -1,14 +1,24 @@
 <?php
     // ESTO NO ESTÁ BIEN HECHO!!
-    // Realmente esto tiene que estar vinculado a una base de datos.
+    // En realidad, esto tendría que estar vinculado a una base de datos.
 
-    $user = "admin"; // Username
-    $password = "root"; // Contraseña
+    // Array de usuarios
+    $users = array("admin", "admin2"); 
+    // Array de contraseñas
+    $passwords = array("root", "root2"); 
 
     $PUser = $_POST["username"];
     $PPass = $_POST["password"];
 
-    if ($PUser == $user && $PPass = $password){
+    $Logueado = false;
+
+    for ($i=0;$i<count($users);$i++){
+        if ($PUser == $users[$i] && $PPass == $passwords[$i]){
+            $Logueado = true;
+        }
+    }
+
+    if ($Logueado){
         echo "Autenticación exitosa.<br>";
         for ($i=1;$i<=4;$i++){
             if (file_exists("../galeria/" . strval($i) . ".jpg") == true)
